bcac064f09cae882ed4461145531355b7de7d378
dovydasp83 <p.dovydas135@gmail.com>
2026-05-25 23:06:11 +0300

    use html

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>You've Entered My Website</h1><p>What a mistake you've made.</p>"
-
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
