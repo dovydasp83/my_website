3c22c41d5a14ffd4fb572a4ac7058596688f1dc1
dovydasp83 <p.dovydas135@gmail.com>
2026-05-25 22:44:48 +0300

    changed web

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Go Away Boy."
+    return "<h1>You've Entered My Website</h1><p>What a mistake you've made.</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
