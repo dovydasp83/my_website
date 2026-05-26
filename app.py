cdfaa1d49498098665f9283f6e309f02190e4208
dovydasp83 <p.dovydas135@gmail.com>
2026-05-26 11:32:38 +0300

    contact

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,7 +9,20 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+    
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        message = request.form["message"]
 
+        return render_template(
+            "contact.html",
+            success = True,
+            name = name
+        )
+    return render_template("contact.html", success=False)
 
 if __name__ == '__main__':
     app.run(debug=True)
